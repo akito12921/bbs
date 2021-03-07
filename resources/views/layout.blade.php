1a54d76e418bcb0d03050804126cfc5ed645f005
kimo.taku <techsan8877@gmail.com>
2021-03-07 04:55:53 +0000

    hikaku-navi

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
     <meta charset="UTF-8">
     <title>RMT BBS</title>
-
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
     <link
         rel="stylesheet"
         href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
